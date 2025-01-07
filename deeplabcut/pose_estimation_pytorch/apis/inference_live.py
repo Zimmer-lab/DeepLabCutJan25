c52feb86f573444616cfc05bbb10134540fe5a06
FabioNaecht <fabio.naecht@univie.ac.at>
2025-01-07 16:47:57 +0100

    benchmarking DLC src live inference on threadripper

--- deeplabcut/pose_estimation_pytorch/apis/inference_live.py
+++ deeplabcut/pose_estimation_pytorch/apis/inference_live.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import imageio as iio
+import numpy as np
 import time
 
 from deeplabcut.pose_estimation_pytorch.task import Task
@@ -8,12 +9,14 @@ from deeplabcut.pose_estimation_pytorch.apis.utils import get_inference_runners
 
 if __name__ == '__main__':
 
-    train_dir = Path("/Users/fabionaecht/Documents/PhD/dlc/tensorflow_pytorch/refined/20230424_nn200_tip-sam/dlc-models-pytorch/iteration-1/20230424_nn200_tipApr24-trainset95shuffle1/train/")
+    # train_dir = Path("/Users/fabionaecht/Documents/PhD/dlc/tensorflow_pytorch/refined/20230424_nn200_tip-sam/dlc-models-pytorch/iteration-1/20230424_nn200_tipApr24-trainset95shuffle1/train/")
+    train_dir = Path("/home/zimadmin/Documents/20230424_nn200_tip-sam/dlc-models-pytorch/iteration-1/20230424_nn200_tipApr24-trainset95shuffle1/train/")
     pytorch_config_path = train_dir / "pytorch_config.yaml"
     snapshot_path = train_dir / "snapshot-200.pt"
 
     # video and inference parameters
-    video_path = Path("/Users/fabionaecht/Documents/PhD/dlc/tensorflow_pytorch/refined/evaluation_custom/200_frames_2023_04_22_15_00_tracking_trial_control_fabio_image_stack5-1.avi")
+    # video_path = Path("/Users/fabionaecht/Documents/PhD/dlc/tensorflow_pytorch/refined/evaluation_custom/200_frames_2023_04_22_15_00_tracking_trial_control_fabio_image_stack5-1.avi")
+    video_path = Path("/home/zimadmin/Documents/20230424_nn200_tip-sam/videos/2023_04_22_15_00_tracking_trial_control_fabio_image_stack1.avi")
     max_num_animals = 1
     batch_size = 16
 
@@ -44,11 +47,15 @@ if __name__ == '__main__':
     print(f"Total number of frames: {frame_number_total}")
 
     # run inference
-    for frame_number in range(20):
+    inference_delays = []
+    for frame_number in range(200):
         print(f"#{frame_number}")
         image = video_reader.get_data(frame_number)
         start_time = time.time()
         predictions = pose_runner.inference([image])
-        print(f"Inference time: {1_000 * (time.time() - start_time):.0f} ms")
+        end_time = time.time()
+        inference_delays.append(end_time - start_time)
+        print(f"Inference time: {1_000 * (end_time- start_time):.0f} ms")
         print(f"Predictions: {predictions}")
     video_reader.close()
+    print(f"Average inference time: {1_000 * np.mean(inference_delays):.1f} ms, median: {1_000 * np.median(inference_delays):.1f} ms")
